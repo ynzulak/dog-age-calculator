097247ff1659f94df7f51bade948dfe2ca077aa5
[ynzulak] <maksymilian.a.kaluzny@gmail.com>
2023-07-22 00:23:54 +0200

    Added html to app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,48 @@
-import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+
+<div className='main-container'>
+  <div className='calculator-container'>
+    <div className='title'>
+      <h1>Dog age calculator</h1>
+      </div>
+    <div className='dog-size-container'>
+      <div className='dog-size-title'><h4>Dog size</h4></div>
+      <div className='dog-sizes'>
+        <div className='dog-size'>1</div>
+        <div className='dog-size'>2</div>
+        <div className='dog-size'>3</div>
+        <div className='dog-size'>4</div>
+        <div className='dog-size'>5</div>
+      </div>
+    </div>
+    <div className='dog-age-container'>
+      <div className='dog-age-title'><h4>Dog age</h4></div>
+      <div className='dog-age-form'>
+        <input type='number' min='0' max='20' placeholder='0'></input>
+      </div>
+      <div className='check-btn'>
+        <button className='check'>Check</button>
+      </div>
+    </div>
+  </div>
+  <div className='calculator-result'>
+    <div className='age-result-container'>
+      <div className='age-result-title'><h4>Result</h4></div>
+      <div className='dog-size-result'>
+        <div className='dog-result-img'>img</div>
+        <div className='result-description'>
+<p>1</p>
+<p>1</p>
+<p>1</p>
+<p>1</p>
         </div>
       </div>
+    </div>
+  </div>
+</div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
   )
 }
