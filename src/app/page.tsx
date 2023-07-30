1aa2ff830b729816943574e46052553c78109dec
[ynzulak] <maksymilian.a.kaluzny@gmail.com>
2023-07-30 20:07:49 +0200

    Almost finished styling app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import Image from 'next/image'
-import miniature from '../images/miniature.jpg'
-import small from '../images/small.jpg'
-import medium from '../images/medium.jpg'
-import big from '../images/big.jpg'
-import large from '../images/large.jpg'
+import miniature from '../images/miniature.png'
+import small from '../images/small.png'
+import medium from '../images/medium.png'
+import big from '../images/big.png'
+import large from '../images/large.png'
 
 import './styles/main.scss'
 
@@ -22,40 +22,42 @@ function Home() {
 						<div className='dog-size'>
 							<span>Miniature</span>
 							<span>Up to 5kg</span>
-							<Image src={miniature} alt='Maltese' width={100} height={100} />
+							<Image src={miniature} alt='Maltese'  />
 						</div>
 						<div className='dog-size'>
 							<span>Small</span>
 							<span>5kg - 12kg</span>
-							<Image src={small} alt='Maltese' width={100} />
+							<Image src={small} alt='Maltese'  />
 						</div>
 						<div className='dog-size'>
 							<span>Medium</span>
 							<span>12kg - 23kg</span>
-							<Image src={medium} alt='Maltese' width={100} />
+							<Image src={medium} alt='Maltese'  />
 						</div>
 						<div className='dog-size'>
 							<span>Big</span>
 							<span>23kg - 45kg</span>
-							<Image src={big} alt='Maltese' width={100} />
+							<Image src={big} alt='Maltese'  />
 						</div>
 						<div className='dog-size'>
 							<span>Large</span>
 							<span>From 45kg</span>
-							<Image src={large} alt='Maltese' width={100} />
+							<Image src={large} alt='Maltese'  />
 						</div>
 					</div>
 					<div className='dog-age-container'>
 						<div className='dog-age-title'>
 							<h3>Dog age</h3>
 						</div>
-						<div className='dog-age-form'>
-							<input type='number' min='1' max='20' placeholder='0'></input>
-							<span>years</span>
-						</div>
-						<div className='dog-age-form'>
-							<input type='number' min='0' max='11' placeholder='0'></input>
-							<span>months</span>
+						<div className="dog-input-form">
+							<div className='dog-age-form'>
+								<input type='number' min='0' max='20' placeholder='0'></input>
+								<span>years</span>
+							</div>
+							<div className='dog-age-form'>
+								<input type='number' min='0' max='11' placeholder='0'></input>
+								<span>months</span>
+							</div>
 						</div>
 						<div className='check-btn'>
 							<button className='check'>Check</button>
@@ -70,7 +72,7 @@ function Home() {
 					</div>
 					<div className='dog-size-result'>
 						<div className='dog-result-img'>
-							<Image src={miniature} alt='Maltese' width={100} height={100} />
+							<Image src={miniature} alt='Maltese'  />
 						</div>
 						<div className='result-description'>
 							<p>Smaller dogs usually live longer than bigger races</p>
