16e3a4023ed88c646e12885344bff1b738995d47
[ynzulak] <maksymilian.a.kaluzny@gmail.com>
2023-08-10 14:48:21 +0200

    Implemented smaller dogs <=3 age logic

--- components/calculations/youngAge.tsx
+++ components/calculations/youngAge.tsx
@@ -1,15 +1,27 @@
-function youngAge(index: number, years: number, months: number) {
-    
-    function sixMonths (humanYears: number) {
-        months 
+import { useState } from "react";
+
+function DogAgeConverter(index: number) {
+    const [dogYears, setDogYears] = useState(0);
+    const [dogMonths, setDogMonths] = useState(0);
+    const [humanYears, setHumanYears] = useState(0);
+
+    const convertToHumanYears = () => {
+        const totalDogYears = dogYears + (dogMonths / 12);
+        let humanAge: number 
+
+        if (totalDogYears <= 0.5) {
+            humanAge = Math.round(totalDogYears * 20);
+          } else if (totalDogYears > 0.5 || totalDogYears <= 1){
+            humanAge = Math.round(totalDogYears * 16);
+        }else if (totalDogYears < 1 || totalDogYears <= 2){
+            humanAge = Math.round(totalDogYears * 12);
+        }else {
+            humanAge = Math.round(24 + (totalDogYears ({} * {dogSizeAge})));
+        }
+
+        setHumanYears(humanAge);
+      };
+
 }
 
-if(years == 0 || months == 6) {
-
-
-
-}
-    
-}
-
-export default youngAge
+export default DogAgeConverter
