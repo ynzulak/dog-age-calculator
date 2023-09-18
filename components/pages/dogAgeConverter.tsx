9cfa4df6af4e2a45888af35a264f4b82bce9a10d
[ynzulak] <maksymilian.a.kaluzny@gmail.com>
2023-09-18 21:05:55 +0200

    Putting app on server

--- components/pages/dogAgeConverter.tsx
+++ components/pages/dogAgeConverter.tsx
@@ -36,7 +36,7 @@ function DogAgeConverter({dogsData}: {dogsData: any}) {
                 <h3>Dog size</h3>
               </div>
             <div className='dog-sizes'>
-              {dogsData.map((element: { title: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | PromiseLikeOfReactNode | null | undefined; description: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | PromiseLikeOfReactNode | null | undefined; imagePath: string | StaticImport; breed: string; }, idx: Key | null | undefined) => (
+              {dogsData.map((element: { title: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | PromiseLikeOfReactNode | null | undefined; description: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | PromiseLikeOfReactNode | null | undefined; imagePath: string | StaticImport; breed: string; }, idx: any) => (
                  <div
                   key={idx}
                   onClick={() => {
